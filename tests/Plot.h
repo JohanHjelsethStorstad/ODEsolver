cd72d9c46cb4d14f3f29d1adcff19335dc1d439e
JohanHjelsethStorstad <johan.h.storstad@gmail.com>
2024-07-29 11:00:16 +0200

    fix: redefinition off Plot::PlotPoints

--- tests/Plot.h
+++ tests/Plot.h
@@ -7,15 +7,7 @@ namespace ODE::Tests {
     public:
         static void plotArrows(const std::vector<Structures::Arrow<double>>& arrows);
         static void plotArrow(const Structures::Arrow<double>& arrow);
-        static void plotPoints(const std::vector<Structures::Point<double>>& points) {
-            std::vector<double> x;
-            std::vector<double> y;
-            for (const Structures::Point<double>& point : points) {
-                x.push_back(point.x);
-                y.push_back(point.y);
-            }
-            matplotlibcpp::plot(x, y);
-        }
+        static void plotPoints(const std::vector<Structures::Point<double>>& points);
         static inline void show() {
             matplotlibcpp::show();
         }
