41406c6e37653a8bb31961663f2a1f5846e11faa
JohanHjelsethStorstad <johan.h.storstad@gmail.com>
2024-08-06 10:51:27 +0200

    chore: adds FieldGeneratorInfo to the config

--- tests/TestConfig.h
+++ tests/TestConfig.h
@@ -3,12 +3,15 @@
 #include "../src/structures/Point.h"
 #include "../src/ButcherTableau.h"
 #include "../src/PrimeFieldInferation.h"
+#include "../src/ODE.h"
 #include <vector>
 #include <string>
 #include <memory>
 #include <map>
 
 namespace ODE::Tests {
+    struct FieldGeneratorInfo;
+
     class TestConfig {
     private:
         const std::map<std::string, std::shared_ptr<BucherTableau::ButcherTableau>> availableTableaus = {
@@ -26,12 +29,44 @@ namespace ODE::Tests {
         double dt = 0.2;
         double fieldDelta = 0.1;
         FieldGeneratorWindow window;
-        FieldGeneratorRandomBounds bounds;
         std::string name = "Test";
         std::vector<Structures::Point<double>> startPoints;
         std::vector<std::shared_ptr<BucherTableau::ButcherTableau>> schemes;
         std::vector<std::shared_ptr<PrimeFieldInferation::PrimeFieldInferationScheme>> inferationSchemes;
         TestConfig() = default;
         const TestConfig& loadFromFile(std::string configFileName);
+        FieldGeneratorInfo fieldGeneratorInfo;
+    };
+
+    enum class FieldGeneratorType {
+        RANDOM,
+        FUNCTION,
+        LOAD_FROM_FILE
+    };
+
+    const std::map<std::string, FieldGeneratorType> FieldGeneratorTypeMap = {
+        {"Random", FieldGeneratorType::RANDOM},
+        {"Function", FieldGeneratorType::FUNCTION},
+        {"LOAD_FROM_FILE", FieldGeneratorType::LOAD_FROM_FILE}
+    };
+
+    const std::map<std::string, ODE<2>> PreProgrammedODEMap = {
+        {"HarmonicOcillator", ODE<2>::HarmonicOscillator(1)},
+        {"Circle", ODE<2>::Circle()},
+        {"Identity", ODE<2>::Identity()},
+        {"CrossIdentity", ODE<2>::CrossIdentity()}
+    };
+
+    struct FieldGeneratorInfo {
+        FieldGeneratorType type;
+
+        //For RANDOM
+        FieldGeneratorRandomBounds bounds;
+
+        //For FUNCTION
+        ODE<2> ode;
+
+        //For LOAD_FROM_FILE
+        std::string fileName;
     };
 }
